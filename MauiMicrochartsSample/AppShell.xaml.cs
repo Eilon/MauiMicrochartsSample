0ae5e42e691d82e7454a2dcc388465c917095d24
Eilon Lipton <Eilon@users.noreply.github.com>
2023-05-24 21:18:25 -0700

    Initial commit

---
+++ MauiMicrochartsSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiMicrochartsSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiMicrochartsSample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiMicrochartsSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
